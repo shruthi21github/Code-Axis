d259cba4dad46e3a2a935dd10e35d2fa8ff2a91d
Rohan-1405 <rrohan3583@gmail.com>
2026-05-07 17:21:15 +0530

    Delete RefreshTokenRepository.java

--- RefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeaxis.repository;
-
-import java.util.Optional;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
-    Optional<RefershToken> findByToken(String token);
-    Optional<RefreshToken> findByUser(String user);
-}
