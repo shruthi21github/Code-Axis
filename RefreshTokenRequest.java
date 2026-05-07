b05f423e7b06c3f126de6eb4077c46207311bcab
Rohan-1405 <rrohan3583@gmail.com>
2026-05-07 17:25:03 +0530

    Delete RefreshTokenRequest.java

--- RefreshTokenRequest.java
+++
@@ -1,9 +0,0 @@
-package com.codeaxis.dto.request;
-
-import lombok.Data;
-
-@Data
-public class RefreshTokenRequest {
-    @NotBlank(message = "Refresh token must not be blank")
-    private String refreshToken;
-}
